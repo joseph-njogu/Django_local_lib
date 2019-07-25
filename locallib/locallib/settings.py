f3d7dd9cf2d16335cc83db44aa9560f88d443bdc
joseph-njogu <josephnjogu487@gmail.com>
2019-07-25 10:24:29 +0300

    debugging environmenatal variables

--- locallib/locallib/settings.py
+++ locallib/locallib/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
-import django
-django.setup()
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
+# import django
+# django.setup()
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 # import os
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.setdefault('^=hclk2qfsdfghj7y6ttredrtgui7u6rdfgghjkojhgfdf#f77f*v#6xcvh4$_ch849v4cruhgxa(wbq70(&0a2w')
+SECRET_KEY = '^=hclk2qfsdfghj7y6ttredrtgui7u6rdfgghjkojhgfdf#f77f*v#6xcvh4$_ch849v4cruhgxa(wbq70(&0a2w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
