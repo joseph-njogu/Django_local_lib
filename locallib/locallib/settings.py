d38ef08d482c80689894e1af6c539f7a75226fc4
joseph-njogu <josephnjogu487@gmail.com>
2019-07-25 10:03:12 +0300

    normalizing

--- locallib/locallib/settings.py
+++ locallib/locallib/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
-# import django
-# django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
+import django
+django.setup()
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 # import os
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^=hclk2qfghjkojhgfdf#f77f*v#6xcvh4$_ch849v4cruhgxa(wbq70(&0a2w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
