069ccd80c38f76dba2688e84ee8b2f0213db5d6c
joseph-njogu <josephnjogu487@gmail.com>
2019-08-06 15:35:44 +0300

    Resolved the failing coverage threshold of 100

--- locallib/catalog/views.py
+++ locallib/catalog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # # from django.shortcuts import get_object_or_404
 # from django.http import HttpResponseRedirect
 # from django.urls import reverse
-# import datetime
+import datetime
 from django.contrib.auth.decorators import permission_required
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -19,32 +19,29 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    # Available copies of books
+   # # Available copies of books
     num_instances_available = \
         BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # The 'all()' is implied by default.
 
-    # # Number of visits to this view, as counted in the session variable.
-    #  num_visits = request.session.get('num_visits', 0)
-    #  request.session['num_visits'] = num_visits+1
+    # Number of visits to this view, as counted in the session variable.
+    # num_visits = request.session.get('num_visits', 0)
+    # request.session['num_visits'] = num_visits + 1
 
     # Render the HTML template index.html
     # with the data in the context variable.
-    return render(
-        request,
-        'index.html',
-        context={
-                'num_books': num_books,
-                'num_instances': num_instances,
-                'num_instances_available': num_instances_available,
-                'num_authors': num_authors, }
+    return render(request,'index.html',context={
+                                                'num_books': num_books,
+                                                'num_instances': num_instances,
+                                                'num_instances_available': num_instances_available,
+                                                'num_authors': num_authors, }
     )
 
 
 class BookListView(generic.ListView):
     """Generic class-based view for a list of books."""
     model = Book
-    paginate_by = 10
+    paginate_by = 2
 
 
 class BookDetailView(generic.DetailView):
@@ -55,21 +52,21 @@ class BookDetailView(generic.DetailView):
 class AuthorListView(generic.ListView):
     """Generic class-based list view for a list of authors."""
     model = Author
-    paginate_by = 10
+    paginate_by = 2
 
 
 class AuthorDetailView(generic.DetailView):
     """Generic class-based detail view for an author."""
     model = Author
 
-
+@permission_required('catalog.can_mark_returned')
 class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
     # Generic class-based view listing all books
     # on loan. Only visible to users with can_mark_returned permission."""
     model = BookInstance
-    # permission_required = 'catalog.can_mark_returned'
-    # template_name = 'catalog/bookinstance_list_borrowed_all.html'
-    # paginate_by = 10
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 2
 
     # def get_queryset(self):
     #     return BookInstance.objects.filter(
@@ -86,7 +83,6 @@ class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
 # from catalog.forms import RenewBookForm
 
 
-# @permission_required('catalog.can_mark_returned')
 # def renew_book_librarian(request, pk):
 #     """View function for renewing a specific BookInstance by librarian."""
 #     book_instance = get_object_or_404(BookInstance, pk=pk)
