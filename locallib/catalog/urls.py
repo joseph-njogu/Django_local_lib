6b222b8bcd37f896f4801982a29c865c334b6663
joseph-njogu <josephnjogu487@gmail.com>
2019-08-15 11:23:37 +0300

    Remove the deploy to server workflow
    Server suspended, that is deploy to it will fail on circleci, thus
    removed it from the workflow

--- locallib/catalog/urls.py
+++ locallib/catalog/urls.py
@@ -11,18 +11,6 @@ urlpatterns = [
        ]
 
 
-# urlpatterns += [
-#     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-#     path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  # Added for challenge
-# ]
-
-
-# Add URLConf for librarian to renew a book.
-# urlpatterns += [
-#     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
-# ]
-
-
 # Add URLConf to create, update, and delete authors
 urlpatterns += [
     path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
